6534883235abf39c26f32bd669bacb1d74c61998
KOngTNP <tanapollll23@gmail.com>
2023-03-30 10:39:50 +0700

    Initial Commit

---
+++ NewBMICalculatorUITests/NewBMICalculatorUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  NewBMICalculatorUITestsLaunchTests.swift
+//  NewBMICalculatorUITests
+//
+//  Created by KOng's Macbook Pro on 30/3/2566 BE.
+//
+
+import XCTest
+
+final class NewBMICalculatorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
